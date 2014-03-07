d8b536874d63f72fe2fd46faef95d0c12bef0512
Patrik Karlsson <patrik@trixon.se>
2014-03-07 13:41:48 +0100

    New phrase

--- modules/se.trixon.almond/src/se/trixon/almond/dictionary/Dict.java
+++ modules/se.trixon.almond/src/se/trixon/almond/dictionary/Dict.java
@@ -13,27 +13,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package se.trixon.almond.dictionary;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
-import org.openide.util.NbBundle;
-
-/**
- *
- * @author Patrik Karlsson <patrik@trixon.se>
- */
-public enum Dict {
-
-    CATEGORY, CONTAINS, COPYRIGHT, DATE, DESCRIPTION, DESTINATION, ENDS_WITH, FILE, FILE_ERROR_READ_MESSAGE, IO_ERROR_TITLE, FILE_ERROR_WRITE_MESSAGE, FILE_EXISTS_MESSAGE, FILE_EXISTS_TITLE, FILE_NOT_FOUND_MESSAGE, FILE_NOT_FOUND_TITLE, INFORMATION, LICENSE, NAME, OPERATION_COMPLETED, PATH, PROJECT, ROWS_NOT_SELECTED_MESSAGE, ROWS_NOT_SELECTED_TITLE, SEARCHING_IN, SIZE, SOURCE, STARTS_WITH, TASK_COMPLETED, TYPE, VERSION;
-    private final ResourceBundle mDefaultResourceBundle = ResourceBundle.getBundle("se/trixon/almond/dictionary/Bundle", Locale.getDefault());
-    private final ResourceBundle mResourceBundle = NbBundle.getBundle(Dict.class);
-
-    public String getString() {
-        return mResourceBundle.getString(name().toLowerCase());
-    }
-
-    public String getStringDefault() {
-        return mDefaultResourceBundle.getString(name().toLowerCase());
-    }
-}
+package se.trixon.almond.dictionary;
+
+
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.openide.util.NbBundle;
+
+
+
+/**
+
+ *
+
+ * @author Patrik Karlsson <patrik@trixon.se>
+
+ */
+
+public enum Dict {
+
+
+
+    CATEGORY, CLEAR, CONTAINS, COPYRIGHT, DATE, DESCRIPTION, DESTINATION, ENDS_WITH, FILE, FILE_ERROR_READ_MESSAGE, IO_ERROR_TITLE, FILE_ERROR_WRITE_MESSAGE, FILE_EXISTS_MESSAGE, FILE_EXISTS_TITLE, FILE_NOT_FOUND_MESSAGE, FILE_NOT_FOUND_TITLE, INFORMATION, LICENSE, NAME, OPERATION_COMPLETED, PATH, PROJECT, ROWS_NOT_SELECTED_MESSAGE, ROWS_NOT_SELECTED_TITLE, SAVE, SEARCHING_IN, SIZE, SOURCE, STARTS_WITH, TASK_COMPLETED, TYPE, VERSION;
+
+    private final ResourceBundle mDefaultResourceBundle = ResourceBundle.getBundle("se/trixon/almond/dictionary/Bundle", Locale.getDefault());
+
+    private final ResourceBundle mResourceBundle = NbBundle.getBundle(Dict.class);
+
+
+
+    public String getString() {
+
+        return mResourceBundle.getString(name().toLowerCase());
+
+    }
+
+
+
+    public String getStringDefault() {
+
+        return mDefaultResourceBundle.getString(name().toLowerCase());
+
+    }
+
+}
+
