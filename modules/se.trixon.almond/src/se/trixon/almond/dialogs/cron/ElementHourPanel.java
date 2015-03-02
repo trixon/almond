0122ef4c099e74043f8b4e3b051c408d2bc49e1e
Patrik Karlsson <patrik@trixon.se>
2015-03-02 22:00:14 +0100

    Schedule dialog

---
+++ modules/se.trixon.almond/src/se/trixon/almond/dialogs/cron/ElementHourPanel.java
@@ -0,0 +1,50 @@
+/* 
+ * Copyright 2015 Patrik Karlsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.almond.dialogs.cron;
+
+/**
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public class ElementHourPanel extends ElementPanel {
+
+    /**
+     * Creates new form ElementHourPanel
+     */
+    public ElementHourPanel() {
+        init();
+    }
+
+    @Override
+    public String getCronString() {
+        return "*";
+    }
+
+    @Override
+    public void setCronString(String cronString) {
+    }
+
+    private void init() {
+        mArray.add("*");
+
+        for (int i = 0; i < 24; i++) {
+            mArray.add(String.valueOf(i));
+        }
+
+        initModel(mArray);
+    }
+
+}
