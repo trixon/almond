be2f3b8fd1887f6558107be5a3dc6848115d0f50
Patrik Karlsson <patrik@trixon.se>
2015-03-21 17:50:27 +0100

    Scale and center image

--- modules/se.trixon.almond/src/se/trixon/almond/imageviewer/ImagePanel.java
+++ modules/se.trixon.almond/src/se/trixon/almond/imageviewer/ImagePanel.java
@@ -21,13 +21,13 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Insets;
 import java.awt.Paint;
 import java.awt.Rectangle;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
+import se.trixon.almond.Scaler;
 
 /**
  *
@@ -35,46 +35,21 @@ import javax.swing.JViewport;
  */
 public class ImagePanel extends JPanel {
 
-    private boolean isTransparentAdd = true;
-    private float mAlignmentX = 0.5f;
-    private float mAlignmentY = 0.5f;
     private Image mImage;
+    private Dimension mImageDimension = new Dimension();
+    private boolean mIsTransparentAdd = true;
     private Paint mPaint;
-    private Style mStyle = Style.SCALED;
 
     public ImagePanel() {
+        super();
     }
 
-    /*
-     *  Set image as the background with the SCALED style
-     */
     public ImagePanel(Image image) {
-        this(image, Style.SCALED);
-    }
-
-    /*
-     *  Set image as the background with the specified style
-     */
-    public ImagePanel(Image image, Style style) {
+        super();
         setImage(image);
-        setStyle(style);
         setLayout(new BorderLayout());
     }
 
-    /*
-     *  Set image as the backround with the specified style and alignment
-     */
-    public ImagePanel(Image image, Style style, float alignmentX, float alignmentY) {
-        setImage(image);
-        setStyle(style);
-        setImageAlignmentX(alignmentX);
-        setImageAlignmentY(alignmentY);
-        setLayout(new BorderLayout());
-    }
-
-    /*
-     *  Use the Paint interface to paint a background
-     */
     public ImagePanel(Paint painter) {
         setPaint(painter);
         setLayout(new BorderLayout());
@@ -91,16 +66,16 @@ public class ImagePanel extends JPanel {
      *  Override method so we can make the component transparent
      */
     public void add(JComponent component, Object constraints) {
-        if (isTransparentAdd) {
+        if (mIsTransparentAdd) {
             makeComponentTransparent(component);
         }
-        
+
         super.add(component, constraints);
     }
 
     /*
      *  Override to provide a preferred size equal to the image size
-    */
+     */
     @Override
     public Dimension getPreferredSize() {
         if (mImage == null) {
@@ -110,43 +85,20 @@ public class ImagePanel extends JPanel {
         }
     }
 
-    /*
-     *	Set the image used as the background
-     */
     public void setImage(Image image) {
-        this.mImage = image;
+        mImage = image;
+
+        if (image == null) {
+            mImageDimension = new Dimension(0, 0);
+        } else {
+            mImageDimension = new Dimension(image.getWidth(null), image.getHeight(null));
+        }
+
         repaint();
     }
 
-    /*
-     *  Specify the horizontal alignment of the image when using ACTUAL style
-     */
-    public void setImageAlignmentX(float alignmentX) {
-        this.mAlignmentX = alignmentX > 1.0f ? 1.0f : alignmentX < 0.0f ? 0.0f : alignmentX;
-        repaint();
-    }
-
-    /*
-     *  Specify the horizontal alignment of the image when using ACTUAL style
-     */
-    public void setImageAlignmentY(float alignmentY) {
-        this.mAlignmentY = alignmentY > 1.0f ? 1.0f : alignmentY < 0.0f ? 0.0f : alignmentY;
-        repaint();
-    }
-
-    /*
-     *	Set the Paint object used to paint the background
-     */
     public void setPaint(Paint painter) {
-        this.mPaint = painter;
-        repaint();
-    }
-
-    /*
-     *	Set the style used to paint the background image
-     */
-    public void setStyle(Style style) {
-        this.mStyle = style;
+        mPaint = painter;
         repaint();
     }
 
@@ -156,7 +108,7 @@ public class ImagePanel extends JPanel {
      *  The default is set to true.
      */
     public void setTransparentAdd(boolean isTransparentAdd) {
-        this.isTransparentAdd = isTransparentAdd;
+        mIsTransparentAdd = isTransparentAdd;
     }
 
     /*
@@ -164,37 +116,18 @@ public class ImagePanel extends JPanel {
      *  The image is positioned in the panel based on the horizontal and
      *  vertical alignments specified.
      */
-    private void drawActual(Graphics g) {
-        Dimension d = getSize();
-        Insets insets = getInsets();
-        int width = d.width - insets.left - insets.right;
-        int height = d.height - insets.top - insets.left;
-        float x = (width - mImage.getWidth(null)) * mAlignmentX;
-        float y = (height - mImage.getHeight(null)) * mAlignmentY;
-        g.drawImage(mImage, (int) x + insets.left, (int) y + insets.top, this);
-    }
+    private void drawImage(Graphics g) {
+        Dimension panelDimension = getSize();
 
-    /*
-     *  Custom painting code for drawing a SCALED image as the background
-     */
-    private void drawScaled(Graphics g) {
-        Dimension d = getSize();
-        g.drawImage(mImage, 0, 0, d.width, d.height, null);
-    }
+        Scaler scaler = new Scaler(new Dimension(mImageDimension));
+        scaler.setHeight(panelDimension.height);
+        scaler.setWidth(panelDimension.width);
+        Dimension scaledImage = scaler.getDimension();
 
-    /*
-     *  Custom painting code for drawing TILED images as the background
-     */
-    private void drawTiled(Graphics g) {
-        Dimension d = getSize();
-        int width = mImage.getWidth(null);
-        int height = mImage.getHeight(null);
-        
-        for (int x = 0; x < d.width; x += width) {
-            for (int y = 0; y < d.height; y += height) {
-                g.drawImage(mImage, x, y, null, null);
-            }
-        }
+        int x = (panelDimension.width - scaledImage.width) / 2;
+        int y = (panelDimension.height - scaledImage.height) / 2;
+
+        g.drawImage(mImage, x, y, scaledImage.width, scaledImage.height, this);
     }
 
     /*
@@ -219,7 +152,7 @@ public class ImagePanel extends JPanel {
     }
 
     /*
-    *  Add custom painting
+     *  Add custom painting
      */
     @Override
     protected void paintComponent(Graphics g) {
@@ -230,6 +163,7 @@ public class ImagePanel extends JPanel {
             Dimension d = getSize();
             Graphics2D g2 = (Graphics2D) g;
             g2.setPaint(mPaint);
+
             g2.fill(new Rectangle(0, 0, d.width, d.height));
         }
 
@@ -238,28 +172,6 @@ public class ImagePanel extends JPanel {
             return;
         }
 
-        switch (mStyle) {
-            case SCALED:
-                drawScaled(g);
-                break;
-
-            case TILED:
-                drawTiled(g);
-                break;
-
-            case ACTUAL:
-                drawActual(g);
-                break;
-
-            default:
-                drawScaled(g);
-        }
-    }
-
-    public enum Style {
-
-        SCALED,
-        TILED,
-        ACTUAL;
+        drawImage(g);
     }
 }
