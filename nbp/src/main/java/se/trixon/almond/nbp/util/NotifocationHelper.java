0687665109816476404def081cd303f5c8a71b0b
Patrik Karlsson <patrik.karlsson@wspgroup.se>
2017-05-12 13:03:47 +0200

    Fix resorce path

--- nbp/src/main/java/se/trixon/almond/nbp/util/NotifocationHelper.java
+++ nbp/src/main/java/se/trixon/almond/nbp/util/NotifocationHelper.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2016 Patrik Karlsson.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,67 +15,41 @@
  */
 package se.trixon.almond.nbp.util;
 
-
-
 import java.util.concurrent.Executors;
-
 import java.util.concurrent.ScheduledExecutorService;
-
 import java.util.concurrent.TimeUnit;
-
 import javax.swing.ImageIcon;
-
 import org.openide.awt.Notification;
-
 import org.openide.awt.NotificationDisplayer;
-
 import org.openide.util.ImageUtilities;
 
-
-
 /**
-
  *
-
+ *
+ *
  * @author Patrik Karlsson
-
+ *
  */
-
 public class NotifocationHelper {
-
-
 
     private static final ScheduledExecutorService sSCHEDULED_EXECUTOR_SERVICE = Executors.newSingleThreadScheduledExecutor();
 
-
-
     public static void displayTextNotification(String title, String text, int autoCloseDelayMilliSeconds) {
 
-        ImageIcon icon = ImageUtilities.loadImageIcon("se/trixon/almond/res/null.png", true);
+        ImageIcon icon = ImageUtilities.loadImageIcon("se/trixon/almond/nbp/res/null.png", true);
 
         final Notification notification = NotificationDisplayer.getDefault().notify(title, icon, text, null);
 
-
-
         if (autoCloseDelayMilliSeconds > 0) {
-
             Runnable task = new Runnable() {
 
                 @Override
-
                 public void run() {
-
                     notification.clear();
-
                 }
-
             };
 
             sSCHEDULED_EXECUTOR_SERVICE.schedule(task, autoCloseDelayMilliSeconds, TimeUnit.MILLISECONDS);
-
         }
-
     }
-
 }
-
