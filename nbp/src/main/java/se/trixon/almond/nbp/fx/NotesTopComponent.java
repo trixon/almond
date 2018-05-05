f96f1abff55789a08a0b46a8e4c9381ceb4931ae
Patrik Karlström <patrik@trixon.se>
2018-05-05 09:44:32 +0200

    Fix persistenceType

--- nbp/src/main/java/se/trixon/almond/nbp/fx/NotesTopComponent.java
+++ nbp/src/main/java/se/trixon/almond/nbp/fx/NotesTopComponent.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2018 pata.
+/* 
+ * Copyright 2018 Patrik Karlström.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ import se.trixon.almond.util.Dict;
 @TopComponent.Description(
         preferredID = "NotesTopComponent",
         //iconBase="SET/PATH/TO/ICON/HERE",
-        persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED
+        persistenceType = TopComponent.PERSISTENCE_ALWAYS
 )
 @TopComponent.Registration(mode = "output", openAtStartup = false)
 @ActionID(category = "Window", id = "se.trixon.almond.nbp.fx.NotesTopComponent")
