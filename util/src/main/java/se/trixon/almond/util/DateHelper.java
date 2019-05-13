331e5866f01e70749c1bf16d005ebb8a41372519
Patrik Karlström <patrik@trixon.se>
2019-05-13 11:08:34 +0200

    Create a DateHelper

---
+++ util/src/main/java/se/trixon/almond/util/DateHelper.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.almond.util;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class DateHelper {
+
+    /**
+     *
+     * returns max of t1 and t2, if equal; t2
+     */
+    public static Date getMax(Date t1, Date t2) {
+        return t1.after(t2) ? t1 : t2;
+    }
+
+    /**
+     *
+     * returns max of t1 and t2, if equal; t2
+     */
+    public static Timestamp getMax(Timestamp t1, Timestamp t2) {
+        return t1.after(t2) ? t1 : t2;
+    }
+
+    /**
+     *
+     * returns min of t1 and t2, if equal; t2
+     */
+    public static Date getMin(Date t1, Date t2) {
+        return t1.before(t2) ? t1 : t2;
+    }
+
+    /**
+     *
+     * returns min of t1 and t2, if equal; t2
+     */
+    public static Timestamp getMin(Timestamp t1, Timestamp t2) {
+        return t1.before(t2) ? t1 : t2;
+    }
+}
