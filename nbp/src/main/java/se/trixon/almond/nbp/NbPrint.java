2d01e2f1446b7beb8c5f8be56ce55504132a7278
Patrik Karlström <patrik@trixon.se>
2019-05-28 17:26:29 +0200

    Add InputOutput writer

---
+++ nbp/src/main/java/se/trixon/almond/nbp/NbPrint.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.almond.nbp;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.SwingUtilities;
+import org.apache.commons.lang3.StringUtils;
+import org.openide.windows.IOProvider;
+import org.openide.windows.InputOutput;
+import org.openide.windows.OutputWriter;
+
+public class NbPrint {
+
+    private SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss.SSS: ");
+    private InputOutput mInputOutput;
+    private boolean mUseTimestamps = true;
+
+    public NbPrint(String title) {
+        SwingUtilities.invokeLater(() -> {
+            mInputOutput = IOProvider.getDefault().getIO(title, false);
+        });
+    }
+
+    public void err(String x) {
+        SwingUtilities.invokeLater(() -> {
+            select();
+            try (OutputWriter outputWriter = mInputOutput.getErr()) {
+                printDate(outputWriter);
+                outputWriter.println(StringUtils.defaultString(x, "NULL"));
+            }
+        });
+    }
+
+    public void err(Object x) {
+        if (x == null) {
+            err("NULL");
+        } else {
+            err(String.valueOf(x));
+        }
+    }
+
+    public synchronized boolean isUseTimestamps() {
+        return mUseTimestamps;
+    }
+
+    public void out(String x) {
+        SwingUtilities.invokeLater(() -> {
+            select();
+            try (OutputWriter outputWriter = mInputOutput.getOut()) {
+                printDate(outputWriter);
+                outputWriter.println(StringUtils.defaultString(x, "NULL"));
+            }
+        });
+    }
+
+    public void out(Object x) {
+        if (x == null) {
+            NbPrint.this.out("NULL");
+        } else {
+            NbPrint.this.out(String.valueOf(x));
+        }
+    }
+
+    public synchronized void select() {
+        mInputOutput.select();
+    }
+
+    public void setDateFormat(SimpleDateFormat dateFormat) {
+        mDateFormat = dateFormat;
+    }
+
+    public void setUseTimestamps(boolean useTimestamps) {
+        mUseTimestamps = useTimestamps;
+    }
+
+    private void printDate(OutputWriter outputWriter) {
+        if (mUseTimestamps) {
+            outputWriter.print(mDateFormat.format(Calendar.getInstance().getTime()));
+        }
+    }
+}
