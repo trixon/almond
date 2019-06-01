6eea1d19ce9008880a6ed1b4f04b694f6d1bdfb5
Patrik Karlström <patrik@trixon.se>
2019-06-01 08:25:51 +0200

    Create DateRangeSlider

---
+++ util/src/main/java/se/trixon/almond/util/fx/control/DateRangeSlider.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.almond.util.fx.control;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import org.controlsfx.control.RangeSlider;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class DateRangeSlider extends RangeSlider {
+
+    private final SimpleObjectProperty<LocalDate> mHighDateProperty = new SimpleObjectProperty<>();
+    private final SimpleObjectProperty<LocalDate> mLowDateProperty = new SimpleObjectProperty<>();
+    private final SimpleObjectProperty<LocalDate> mMaxDateProperty = new SimpleObjectProperty<>();
+    private final SimpleObjectProperty<LocalDate> mMinDateProperty = new SimpleObjectProperty<>();
+
+    public DateRangeSlider() {
+        init();
+        initListeners();
+    }
+
+    public LocalDate getHighDate() {
+        return mHighDateProperty.getValue();
+    }
+
+    public LocalDate getLowDate() {
+        return mLowDateProperty.getValue();
+    }
+
+    public LocalDate getMaxDate() {
+        return mMaxDateProperty.getValue();
+    }
+
+    public LocalDate getMinDate() {
+        return mMinDateProperty.getValue();
+    }
+
+    public SimpleObjectProperty<LocalDate> highDateProperty() {
+        return mHighDateProperty;
+    }
+
+    public SimpleObjectProperty<LocalDate> lowDateProperty() {
+        return mLowDateProperty;
+    }
+
+    public SimpleObjectProperty<LocalDate> maxDateProperty() {
+        return mMaxDateProperty;
+    }
+
+    public SimpleObjectProperty<LocalDate> minDateProperty() {
+        return mMinDateProperty;
+    }
+
+    public void setHighDate(LocalDate localDate) {
+        mHighDateProperty.setValue(localDate);
+    }
+
+    public void setLowDate(LocalDate localDate) {
+        mLowDateProperty.setValue(localDate);
+    }
+
+    public void setMaxDate(LocalDate localDate) {
+        mMaxDateProperty.setValue(localDate);
+    }
+
+    public void setMinDate(LocalDate localDate) {
+        mMinDateProperty.setValue(localDate);
+    }
+
+    public void setMinMaxDate(LocalDate minDate, LocalDate maxDate) {
+        mMinDateProperty.setValue(minDate);
+        mMaxDateProperty.setValue(maxDate);
+
+        mLowDateProperty.setValue(minDate);
+        mHighDateProperty.setValue(maxDate);
+
+        setMin(0);
+        setMax(ChronoUnit.DAYS.between(minDate, maxDate));
+
+        setLowValue(getMin());
+        setHighValue(getMax());
+    }
+
+    private void init() {
+        setMajorTickUnit(1);
+        setMinorTickCount(1);
+    }
+
+    private void initListeners() {
+        lowValueProperty().addListener((ObservableValue<? extends Number> ov, Number t, Number t1) -> {
+            lowDateProperty().setValue(getMinDate().plusDays(t1.intValue()));
+        });
+
+        highValueProperty().addListener((ObservableValue<? extends Number> ov, Number t, Number t1) -> {
+            highDateProperty().setValue(getMinDate().plusDays(t1.intValue()));
+        });
+    }
+
+}
