0d698846705ac51e535aecc5cf9525bee0c9a661
Patrik Karlström <patrik@trixon.se>
2019-06-18 21:45:56 +0200

    Put Notes in editor mode

--- nbp/src/main/java/se/trixon/almond/nbp/fx/NotesHtmlTopComponent.java
+++ nbp/src/main/java/se/trixon/almond/nbp/fx/NotesHtmlTopComponent.java
@@ -39,7 +39,7 @@ import se.trixon.almond.util.Dict;
         //iconBase="SET/PATH/TO/ICON/HERE",
         persistenceType = TopComponent.PERSISTENCE_ALWAYS
 )
-@TopComponent.Registration(mode = "notes", openAtStartup = false)
+@TopComponent.Registration(mode = "editor", openAtStartup = false)
 @ActionID(category = "Window", id = "se.trixon.almond.nbp.fx.NotesHtmlTopComponent")
 @ActionReference(path = "Menu/Window" /*, position = 333 */)
 @TopComponent.OpenActionRegistration(
