a586b745cfe9bc304b7bd5b7ba674f296b684209
Patrik Karlström <patrik@trixon.se>
2019-09-24 17:27:15 +0200

    Fix java doc tag

--- util/src/main/java/se/trixon/almond/util/ext/GrahamScan.java
+++ util/src/main/java/se/trixon/almond/util/ext/GrahamScan.java
@@ -39,15 +39,12 @@ import java.util.TreeSet;
 public final class GrahamScan {
 
     /**
-     * Returns the convex hull of the points created from <code>xs</code> and <code>ys</code>. Note
-     * that the first and last point in the returned <code>List&lt;java.awt.Point&gt;</code> are the
-     * same point.
+     * Returns the convex hull of the points created from <code>xs</code> and <code>ys</code>. Note that the first and last point in the returned <code>List&lt;java.awt.Point&gt;</code> are the same point.
      *
      * @param xs the x coordinates.
      * @param ys the y coordinates.
      * @return the convex hull of the points created from <code>xs</code> and <code>ys</code>.
-     * @throws IllegalArgumentException if <code>xs</code> and <code>ys</code> don't have the same
-     * size, if all points are collinear or if there are less than 3 unique points present.
+     * @throws IllegalArgumentException if <code>xs</code> and <code>ys</code> don't have the same size, if all points are collinear or if there are less than 3 unique points present.
      */
     public static List<Point> getConvexHull(int[] xs, int[] ys) throws IllegalArgumentException {
 
@@ -65,14 +62,11 @@ public final class GrahamScan {
     }
 
     /**
-     * Returns the convex hull of the points created from the list <code>points</code>. Note that
-     * the first and last point in the returned <code>List&lt;java.awt.Point&gt;</code> are the same
-     * point.
+     * Returns the convex hull of the points created from the list <code>points</code>. Note that the first and last point in the returned <code>List&lt;java.awt.Point&gt;</code> are the same point.
      *
      * @param points the list of points.
      * @return the convex hull of the points created from the list <code>points</code>.
-     * @throws IllegalArgumentException if all points are collinear or if there are less than 3
-     * unique points present.
+     * @throws IllegalArgumentException if all points are collinear or if there are less than 3 unique points present.
      */
     public static List<Point> getConvexHull(List<Point> points) throws IllegalArgumentException {
 
@@ -120,7 +114,7 @@ public final class GrahamScan {
 
     /**
      *
-     * @author Patrik Karlström
+     * author Patrik Karlström
      */
     public static List<Point.Double> getConvexHullDouble(List<Point.Double> pointsDouble) throws IllegalArgumentException {
         final double factor = 1000000F;
@@ -168,12 +162,10 @@ public final class GrahamScan {
     }
 
     /**
-     * Returns the points with the lowest y coordinate. In case more than 1 such point exists, the
-     * one with the lowest x coordinate is returned.
+     * Returns the points with the lowest y coordinate. In case more than 1 such point exists, the one with the lowest x coordinate is returned.
      *
      * @param points the list of points to return the lowest point from.
-     * @return the points with the lowest y coordinate. In case more than 1 such point exists, the
-     * one with the lowest x coordinate is returned.
+     * @return the points with the lowest y coordinate. In case more than 1 such point exists, the one with the lowest x coordinate is returned.
      */
     protected static Point getLowestPoint(List<Point> points) {
 
@@ -192,9 +184,7 @@ public final class GrahamScan {
     }
 
     /**
-     * Returns a sorted set of points from the list <code>points</code>. The set of points are
-     * sorted in increasing order of the angle they and the lowest point <tt>P</tt> make with the
-     * x-axis. If tow (or more) points form the same angle towards <tt>P</tt>, the one closest to
+     * Returns a sorted set of points from the list <code>points</code>. The set of points are sorted in increasing order of the angle they and the lowest point <tt>P</tt> make with the x-axis. If tow (or more) points form the same angle towards <tt>P</tt>, the one closest to
      * <tt>P</tt>
      * comes first.
      *
@@ -246,21 +236,17 @@ public final class GrahamScan {
     }
 
     /**
-     * Returns the GrahamScan#Turn formed by traversing through the ordered points <code>a</code>,
-     * <code>b</code> and <code>c</code>. More specifically, the cross product <tt>C</tt> between
-     * the 3 points (vectors) is calculated:
+     * Returns the GrahamScan#Turn formed by traversing through the ordered points <code>a</code>, <code>b</code> and <code>c</code>. More specifically, the cross product <tt>C</tt> between the 3 points (vectors) is calculated:
      *
      * <tt>(b.x-a.x * c.y-a.y) - (b.y-a.y * c.x-a.x)</tt>
      *
      * and if <tt>C</tt> is less than 0, the turn is CLOCKWISE, if
-     * <tt>C</tt> is more than 0, the turn is COUNTER_CLOCKWISE, else the three points are
-     * COLLINEAR.
+     * <tt>C</tt> is more than 0, the turn is COUNTER_CLOCKWISE, else the three points are COLLINEAR.
      *
      * @param a the starting point.
      * @param b the second point.
      * @param c the end point.
-     * @return the GrahamScan#Turn formed by traversing through the ordered points <code>a</code>,
-     * <code>b</code> and <code>c</code>.
+     * @return the GrahamScan#Turn formed by traversing through the ordered points <code>a</code>, <code>b</code> and <code>c</code>.
      */
     protected static Turn getTurn(Point a, Point b, Point c) {
 
