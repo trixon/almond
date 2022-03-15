bd700813a0f68b2af6f61d2b974be0ec4793f1f6
Patrik Karlström <patrik@trixon.se>
2022-03-15 15:31:37 +0100

    util: Add LocalDateTimeAdapter

---
+++ util/src/main/java/se/trixon/almond/util/gson_adapter/LocalDateTimeAdapter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.almond.util.gson_adapter;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+
+    @Override
+    public JsonElement serialize(LocalDateTime t, Type type, JsonSerializationContext jsc) {
+        return jsc.serialize(t.toString());
+    }
+
+    @Override
+    public LocalDateTime deserialize(JsonElement je, Type type, JsonDeserializationContext jdc) throws JsonParseException {
+        return LocalDateTime.parse(je.getAsString());
+    }
+
+}
