7faeb7f6dca83f1c2c15feae58852d5cfc0c4903
Patrik Karlström <patrik@trixon.se>
2023-05-18 08:31:16 +0200

    Update copyright

--- util/src/main/java/se/trixon/almond/util/AlmondActionListener.java
+++ util/src/main/java/se/trixon/almond/util/AlmondActionListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Patrik Karlström <patrik@trixon.se>.
+ * Copyright 2023 Patrik Karlström <patrik@trixon.se>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
