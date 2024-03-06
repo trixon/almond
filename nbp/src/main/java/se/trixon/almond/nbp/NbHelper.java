c113388419253f56ac3275e5d190ddd43eaa436c
Patrik Karlström <patrik@trixon.se>
2024-03-06 15:59:32 +0100

    nbp: Add Laf methods to NbHelper

--- nbp/src/main/java/se/trixon/almond/nbp/NbHelper.java
+++ nbp/src/main/java/se/trixon/almond/nbp/NbHelper.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2023 Patrik Karlström.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,13 +15,25 @@
  */
 package se.trixon.almond.nbp;
 
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.netbeans.api.progress.ProgressHandle;
+import org.openide.util.Exceptions;
+import org.openide.util.NbPreferences;
+import se.trixon.almond.nbp.output.OutputLineMode;
+import se.trixon.almond.util.PrefsHelper;
+import se.trixon.almond.util.fx.FxHelper;
+import se.trixon.almond.util.icons.material.MaterialIcon;
 
 /**
  *
  * @author Patrik Karlström
  */
 public class NbHelper {
+
+    private static final Preferences sLafPreferences = NbPreferences.root().node("laf");
 
     public static ProgressHandle createAndStartProgressHandle(String displayName, boolean indeterminate) {
         var progressHandle = ProgressHandle.createHandle(displayName);
@@ -33,4 +45,41 @@ public class NbHelper {
         return progressHandle;
     }
 
+    public static Preferences getLafPreferences() {
+        return sLafPreferences;
+    }
+
+    public static boolean isNightMode() {
+        return StringUtils.containsIgnoreCase(sLafPreferences.get("laf", ""), "dark");
+    }
+
+    public static void initNightModeIfNeeded() {
+        if (isNightMode()) {
+            FxHelper.setDarkThemeEnabled(true);
+            var color = FxHelper.getFillColorForDarkTheme();
+            MaterialIcon.setDefaultColor(color);
+            se.trixon.almond.util.icons.material.swing.MaterialIcon.setDefaultColor(FxHelper.colorToColor(color));
+
+            OutputLineMode.setNightMode(true);
+        }
+
+    }
+
+    public static void setLafAccentColor(String color) {
+        try {
+            var preferences = NbPreferences.root().node("org/netbeans/swing/laf/flatlaf");
+            PrefsHelper.putIfAbsent(preferences, "accentColor", color);
+        } catch (BackingStoreException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+    public static void setLafDefault(String mode) {
+        try {
+            var defaultLAF = !SystemUtils.IS_OS_MAC ? "com.formdev.flatlaf.Flat" + mode + "Laf" : "com.formdev.flatlaf.themes.FlatMac" + mode + "Laf";
+            PrefsHelper.putIfAbsent(sLafPreferences, "laf", defaultLAF);
+        } catch (BackingStoreException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
 }
